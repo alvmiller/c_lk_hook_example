01bd6eb3db53559bf104ff846b15e94df24966de
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-04-11 21:22:55 +0300

    Create hook_lk.c

---
+++ hook_lk.c
@@ -0,0 +1,271 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/version.h>
+#include <linux/namei.h>
+#include <linux/ftrace.h>
+#include <linux/linkage.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/kprobes.h>
+
+// https://eunomia.dev/en/blogs/inline-hook/
+// https://foxtrot-sq.medium.com/linux-rootkits-multiple-ways-to-hook-syscall-s-7001cc02a1e6
+// https://xcellerator.github.io/posts/linux_rootkits_02/
+// https://classroom.anir0y.in/post/tryhackme-linuxfunctionhooking/
+// https://www.apriorit.com/dev-blog/546-hooking-linux-functions-2
+// https://gist.github.com/xcellerator/ac2c039a6bbd7782106218298f5e5ac1
+// https://nixhacker.com/hooking-syscalls-in-linux-using-ftrace/
+
+// make
+// sudo dmesg -C
+// sudo insmod hook_lk.ko
+// mkdir lol
+// sudo dmesg
+// sudo rmmod hook_lk.ko
+
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+#ifdef PTREGS_SYSCALL_STUBS
+#define SYSCALL_NAME(name) ("__x64_" name)
+#else
+#define SYSCALL_NAME(name) (name)
+#endif
+
+#define HOOK(_name, _hook, _orig)   \
+{                   \
+    .name = SYSCALL_NAME(_name),        \
+    .function = (_hook),        \
+    .original = (_orig),        \
+}
+
+#define USE_FENTRY_OFFSET 0
+#if !USE_FENTRY_OFFSET
+#pragma GCC optimize("-fno-optimize-sibling-calls")
+#endif
+
+struct ftrace_hook {
+    const char *name;
+    void *function;
+    void *original;
+
+    unsigned long address;
+    struct ftrace_ops ops;
+};
+
+int fh_install_hook(struct ftrace_hook *hook);
+void fh_remove_hook(struct ftrace_hook *hook);
+int fh_install_hooks(struct ftrace_hook *hooks, size_t count);
+void fh_remove_hooks(struct ftrace_hook *hooks, size_t count);
+#ifdef PTREGS_SYSCALL_STUBS
+asmlinkage int hook_mkdir(const struct pt_regs *regs);
+#else
+asmlinkage int hook_mkdir(const char __user *pathname, umode_t mode);
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5,11,0)
+#define FTRACE_OPS_FL_RECURSION FTRACE_OPS_FL_RECURSION_SAFE
+#define ftrace_regs pt_regs
+static __always_inline struct pt_regs *ftrace_get_regs(struct ftrace_regs *fregs)
+{
+	return fregs;
+}
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0)
+static unsigned long lookup_name(const char *name)
+{
+	struct kprobe kp = {
+		.symbol_name = name
+	};
+	unsigned long retval;
+
+	if (register_kprobe(&kp) < 0) return 0;
+	retval = (unsigned long) kp.addr;
+	unregister_kprobe(&kp);
+	return retval;
+}
+#else
+static unsigned long lookup_name(const char *name)
+{
+	return kallsyms_lookup_name(name);
+}
+#endif
+
+static int fh_resolve_hook_address(struct ftrace_hook *hook)
+{
+    //hook->address = kallsyms_lookup_name(hook->name);
+    hook->address = lookup_name(hook->name);
+
+    if (!hook->address)
+    {
+        printk(KERN_DEBUG "rootkit: unresolved symbol: %s\n", hook->name);
+        return -ENOENT;
+    }
+
+#if USE_FENTRY_OFFSET
+    *((unsigned long*) hook->original) = hook->address + MCOUNT_INSN_SIZE;
+#else
+    *((unsigned long*) hook->original) = hook->address;
+#endif
+
+    return 0;
+}
+
+static void notrace fh_ftrace_thunk(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct ftrace_regs *fregs)
+//static void notrace fh_ftrace_thunk(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct pt_regs *regs)
+//static void notrace fh_ftrace_thunk(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct ftrace_regs *regs)
+{
+    struct pt_regs *regs = ftrace_get_regs(fregs);
+
+    struct ftrace_hook *hook = container_of(ops, struct ftrace_hook, ops);
+
+#if USE_FENTRY_OFFSET
+    regs->ip = (unsigned long) hook->function;
+#else
+    if(!within_module(parent_ip, THIS_MODULE))
+        regs->ip = (unsigned long) hook->function;
+#endif
+}
+
+int fh_install_hook(struct ftrace_hook *hook)
+{
+    int err;
+    err = fh_resolve_hook_address(hook);
+    if(err)
+        return err;
+    hook->ops.func = fh_ftrace_thunk;
+    hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_RECURSION | FTRACE_OPS_FL_IPMODIFY;
+    //hook->ops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY;
+
+    err = ftrace_set_filter_ip(&hook->ops, hook->address, 0, 0);
+    if(err)
+    {
+        printk(KERN_DEBUG "rootkit: ftrace_set_filter_ip() failed: %d\n", err);
+        return err;
+    }
+
+    err = register_ftrace_function(&hook->ops);
+    if(err)
+    {
+        printk(KERN_DEBUG "rootkit: register_ftrace_function() failed: %d\n", err);
+        return err;
+    }
+
+    return 0;
+}
+
+void fh_remove_hook(struct ftrace_hook *hook)
+{
+    int err;
+    err = unregister_ftrace_function(&hook->ops);
+    if(err)
+    {
+        printk(KERN_DEBUG "rootkit: unregister_ftrace_function() failed: %d\n", err);
+    }
+
+    err = ftrace_set_filter_ip(&hook->ops, hook->address, 1, 0);
+    if(err)
+    {
+        printk(KERN_DEBUG "rootkit: ftrace_set_filter_ip() failed: %d\n", err);
+    }
+}
+
+int fh_install_hooks(struct ftrace_hook *hooks, size_t count)
+{
+    int err;
+    size_t i;
+
+    for (i = 0 ; i < count ; i++)
+    {
+        err = fh_install_hook(&hooks[i]);
+        if(err)
+            goto error;
+    }
+    return 0;
+
+error:
+    while (i != 0)
+    {
+        fh_remove_hook(&hooks[--i]);
+    }
+    return err;
+}
+
+void fh_remove_hooks(struct ftrace_hook *hooks, size_t count)
+{
+    size_t i;
+
+    for (i = 0 ; i < count ; i++)
+        fh_remove_hook(&hooks[i]);
+}
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("mkdir syscall hook");
+MODULE_VERSION("0.01");
+
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+#ifdef PTREGS_SYSCALL_STUBS
+static asmlinkage long (*orig_mkdir)(const struct pt_regs *);
+
+asmlinkage int hook_mkdir(const struct pt_regs *regs)
+{
+    char __user *pathname = (char *)regs->di;
+    char dir_name[NAME_MAX] = {0};
+
+    printk(KERN_INFO "rootkit: Try...\n");
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if (error > 0)
+        printk(KERN_INFO "rootkit: trying to create directory with name: %s\n", dir_name);
+
+    orig_mkdir(regs);
+    return 0;
+}
+#else
+static asmlinkage long (*orig_mkdir)(const char __user *pathname, umode_t mode);
+
+asmlinkage int hook_mkdir(const char __user *pathname, umode_t mode)
+{
+    char dir_name[NAME_MAX] = {0};
+
+    printk(KERN_INFO "rootkit: Try...\n");
+    long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
+
+    if (error > 0)
+        printk(KERN_INFO "rootkit: trying to create directory with name %s\n", dir_name);
+
+    orig_mkdir(pathname, mode);
+    return 0;
+}
+#endif
+
+static struct ftrace_hook hooks[] = {
+    HOOK("sys_mkdir", hook_mkdir, &orig_mkdir),
+};
+
+static int __init rootkit_init(void)
+{
+    int err;
+    err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+    if(err)
+        return err;
+
+    printk(KERN_INFO "rootkit: loaded\n");
+    return 0;
+}
+
+static void __exit rootkit_exit(void)
+{
+    fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
+    printk(KERN_INFO "rootkit: unloaded\n");
+}
+
+module_init(rootkit_init);
+module_exit(rootkit_exit);
